aed2eda51da3c99e5ed035b8808426612c1f99d8
Ninocabanero <cabaneronino50@gmail.com>
2025-08-26 08:45:59 +0800

    index and create were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#6613aaff',
     color: 'white',
     borderRadius: 8,
   }
